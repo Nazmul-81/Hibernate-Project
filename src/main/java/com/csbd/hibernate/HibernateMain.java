e7239cad9d580cf0c33274fd8761995d6625258d
Nazmul-81 <nazmul81csekuet@gmail.com>
2025-05-11 20:34:33 +0600

    CRUD operation and refactoring the code

--- src/main/java/com/csbd/hibernate/HibernateMain.java
+++ src/main/java/com/csbd/hibernate/HibernateMain.java
@@ -6,51 +6,90 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateMain {
+
+    // Have DB connection poll
+    // Mapping metadata
+    // Caches for optimization
+    // Heavy-weight, thread safe
+    private static final SessionFactory sessionFactory = new Configuration()
+            .addAnnotatedClass(Student.class)
+            .configure("hibernate.cfg.xml")
+            .buildSessionFactory();
+
     public static void main(String[] args) {
+        createStudent("Jhon Kity", 24);
+        createStudent("Err Hood", 25);
 
-        // Have DB connection poll
-        // Mapping metadata
-        // Caches for optimization
-        // Heavy-weight, thread safe
-        SessionFactory sessionFactory = new Configuration()
-                .addAnnotatedClass(Student.class)
-                .configure("hibernate.cfg.xml")
-                .buildSessionFactory();
+        Student student = getStudent(1);
+        System.out.println("Read "+ student);
 
-        Student student1 = new Student();
-        student1.setName("James Bond");
-        student1.setAge(21);
+        updateStudent(2, "Error" , 26);
+        deleteStudent(1);
 
-        Student student2 = new Student();
-        student2.setName("Kail Mayers");
-        student2.setAge(18);
+        sessionFactory.close();
+    }
 
-        Student student3 = new Student();
-        student3.setName("Swift");
-        student3.setAge(23);
-
+    private static void createStudent(String name, int age) {
         // Hibernate tracks object in the persistence context
-        Session session = sessionFactory.openSession();
+        // It represents a persistence context — meaning it tracks your objects
+        // and knows whether they’re new, dirty, or detached.
+        try(Session session = sessionFactory.openSession()) {
 
-        // Some transaction will not persist without commit (insert/update)
-        Transaction tx = session.beginTransaction();
+            // Some transaction will not persist without commit (insert/update)
+            session.beginTransaction();
 
-        // Hibernate queues an insert query
-        session.persist(student1);
-        session.persist(student2);
-        session.persist(student3);
+            Student student = new Student(name, age);
 
-        // Hibernate sends SQL to MySQL, JDBC executes the query
-        tx.commit();
+            // Hibernate queues an insert query
+            // Transaction needed but save do not needed
+            // No ID return but save returns ID
+            // JPA standard but save Hibernate specific
+            session.persist(student);
 
-        // Hibernate update ID by DB generated ID
+            // Hibernate sends SQL to MySQL, JDBC executes the query
+            session.getTransaction().commit();
 
-        // Object will be detached and no longer be tracked
-        session.close();
-        System.out.println(student1);
-        System.out.println(student2);
-        System.out.println(student3);
+            System.out.println(student);
 
+            // Hibernate update ID by DB generated ID
 
+            // Object will be detached and no longer be tracked (finally)
+        }
+    }
+    private static Student getStudent(int id) {
+        try(Session session = sessionFactory.openSession()) {
+            return session.get(Student.class, id);
+        }
+    }
+
+    private static void updateStudent(int roll, String name, int age) {
+        try(Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            Student student = session.get(Student.class, roll);
+            if(student != null) {
+                student.setName(name);
+                student.setAge(age);
+                // Works for detached or non-detached object
+                // Safe use in modern apps
+                // This works for object that is connected to another session but update need detached object
+                session.merge(student);
+            }
+            session.getTransaction().commit();
+
+        }
+    }
+
+    private static void deleteStudent(int id) {
+        try(Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            Student student = session.get(Student.class, id);
+            if(student != null) {
+
+                // Need attached to session
+                // JPA standard but delete Hibernate standard
+                session.remove(student);
+            }
+            session.getTransaction().commit();
+        }
     }
 }
--- src/main/java/com/csbd/hibernate/Student.java
+++ src/main/java/com/csbd/hibernate/Student.java
@@ -1,8 +1,6 @@
 package com.csbd.hibernate;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -10,17 +8,18 @@ import javax.persistence.*;
 @Setter
 @ToString
 @Entity
+@NoArgsConstructor
 public class Student {
     @Id
-    @GeneratedValue(strategy = GenerationType.TABLE)
-    @TableGenerator(
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    /*@TableGenerator(
             name = "hibernate_gen",
             table = "hibernate_sequences",  // Default table name
             pkColumnName = "sequence_name", // Default column
             valueColumnName = "next_val",   // Default column
             pkColumnValue = "student",      // No "_id" suffix
             allocationSize = 5              // Hibernate cache ids not run select and update query
-    )
+    )*/
     // For TABLE if you do not use this default table and column name then you need to use a configuration
     // IDENTITY: use auto-increment column in MySQL
     // TABLE : Use another table then get next ID before insert. Update this next id in this table after insert.
@@ -29,4 +28,9 @@ public class Student {
     private int roll;
     private String name;
     private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
